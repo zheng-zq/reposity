6271c80ce9d996e95ab20901d452ab0c1fcf61a7
itcast <1137369315@qq.com>
2020-09-28 23:50:20 +0800

    学成在线项目

---
+++ test-fastdfs/src/test/java/com/xuecheng/test/fastdfs/Test01.java
@@ -0,0 +1,13 @@
+package com.xuecheng.test.fastdfs;
+
+/**
+ * @Author ZZQ
+ * @Date 2020/9/28 23:48
+ * @Version 1.0
+ */
+public class Test01 {
+
+    public static void main(String[] args) {
+        System.out.println("20200928 created");
+    }
+}
