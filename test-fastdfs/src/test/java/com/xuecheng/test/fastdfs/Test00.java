52c68ee2b1d29822bd1744d2284e4b85cef9d5ce
zhengzhiqing <18839110393@163.com>
2022-02-18 22:59:28 +0800

    22:59测试

--- test-fastdfs/src/test/java/com/xuecheng/test/fastdfs/Test00.java
+++ test-fastdfs/src/test/java/com/xuecheng/test/fastdfs/Test00.java
@@ -55,6 +55,6 @@ public class Test00 {
         String s = map.toString();
         //转成json
         String jsonString = JSON.toJSONString(map);
-        System.out.println("");
+        System.out.println("测试");
     }
 }
